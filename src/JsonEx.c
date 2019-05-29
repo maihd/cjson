53bb1662bd3723f44fcc068c14369172a2901658
MaiHD <maihd.dev@gmail.com>
2019-05-29 22:08:47 +0700

    experiment temp allocator

---
+++ src/JsonEx.c
@@ -0,0 +1,165 @@
+#include "JsonEx.h"
+
+/* @funcdef: JsonWrite */
+void JsonWrite(const JsonValue* value, FILE* out)
+{
+    if (value)
+    {
+        int i, n;
+
+        switch (value->type)
+        {
+        case JSON_NULL:
+            fprintf(out, "null");
+            break;
+
+        case JSON_NUMBER:
+            fprintf(out, "%lf", value->number);
+            break;
+
+        case JSON_BOOLEAN:
+            fprintf(out, "%s", value->boolean ? "true" : "false");
+            break;
+
+        case JSON_STRING:
+            fprintf(out, "\"%s\"", value->string);
+            break;
+
+        case JSON_ARRAY:
+            fprintf(out, "[");
+            for (i = 0, n = JsonLength(value); i < n; i++)
+            {
+                JsonWrite(value->array[i], out);
+                if (i < n - 1)
+                {
+                    fprintf(out, ",");
+                }
+            }
+            fprintf(out, "]");
+            break;
+
+        case JSON_OBJECT:
+            fprintf(out, "{");
+            for (i = 0, n = JsonLength(value); i < n; i++)
+            {
+#ifdef JSON_OBJECT_KEYNAME
+                fprintf(out, "\"%s\" : ", value->object[i].name);
+#else
+                fprintf(out, "\"<hash: %d>\" : ", value->object[i].hash);
+#endif
+
+                JsonWrite(value->object[i].value, out);
+                if (i < n - 1)
+                {
+                    fprintf(out, ",");
+                }            
+            }
+            fprintf(out, "}");
+            break;
+
+        case JSON_NONE:
+        default:
+            break;
+        }
+    }
+}          
+
+/* @funcdef: JsonPrint */
+void JsonPrint(const JsonValue* value, FILE* out)
+{
+    if (value)
+    {
+        int i, n;
+        static int indent = 0;
+
+        switch (value->type)
+        {
+        case JSON_NULL:
+            fprintf(out, "null");
+            break;
+
+        case JSON_NUMBER:
+            fprintf(out, "%lf", value->number);
+            break;
+
+        case JSON_BOOLEAN:
+            fprintf(out, "%s", value->boolean ? "true" : "false");
+            break;
+
+        case JSON_STRING:
+            fprintf(out, "\"%s\"", value->string);
+            break;
+
+        case JSON_ARRAY:
+            fprintf(out, "[\n");
+
+            indent++;
+            for (i = 0, n = JsonLength(value); i < n; i++)
+            {
+                int j, m;
+                for (j = 0, m = indent * 4; j < m; j++)
+                {
+                    fputc(' ', out);
+                }
+
+                JsonPrint(value->array[i], out);
+                if (i < n - 1)
+                {
+                    fputc(',', out);
+                }
+                fprintf(out, "\n");
+            }
+            indent--;
+
+            for (i = 0, n = indent * 4; i < n; i++)
+            {
+                fprintf(out, " ");
+            }  
+            fputc(']', out);
+            break;
+
+        case JSON_OBJECT:
+            fprintf(out, "{\n");
+
+            indent++;
+            for (i = 0, n = JsonLength(value); i < n; i++)
+            {
+                int j, m;
+                for (j = 0, m = indent * 4; j < m; j++)
+                {
+                    fputc(' ', out);
+                }
+
+#ifdef JSON_OBJECT_KEYNAME
+                fprintf(out, "\"%s\" : ", value->object[i].name);
+#else
+                fprintf(out, "\"<hash: %d>\" : ", value->object[i].hash);
+#endif
+                JsonPrint(value->object[i].value, out);
+                if (i < n - 1)
+                {
+                    fputc(',', out);
+                }
+                fputc('\n', out);
+            }
+            indent--;
+
+            for (i = 0, n = indent * 4; i < n; i++)
+            {                    
+                fputc(' ', out);
+            }                    
+            fputc('}', out);
+            break;
+
+        case JSON_NONE:
+        default:
+            break;
+        }
+    }
+}
+
+int JsonTempAllocator(JsonAllocator* allocator, void* buffer, int length)
+{
+    return 1;
+}
+
