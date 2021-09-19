7889bef9c8cc4be19677f494f2a9e41accc6a112
maihd <maihd.dev@gmail.com>
2021-09-20 01:08:49 +0700

    add examples

---
+++ examples/LDtkParser/LDtkParser.h
@@ -0,0 +1,37 @@
+#pragma once
+
+typedef enum LDtkLayerType
+{
+
+} LDtkLayerType;
+
+typedef struct LDtkEntity
+{
+    
+} LDtkEntity;
+
+typedef struct LDtkTile
+{
+
+} LDtkTile;
+
+typedef struct LDtkTileset
+{
+    
+} LDtkTileset;
+
+typedef struct LDtkLayer
+{
+
+} LDtkLayer;
+
+typedef struct LDtkIntGrid
+{
+    
+} LDtkIntGrid;
+
+typedef struct LDtkWorld
+{
+
+} LDtkWorld;
+
