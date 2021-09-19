d95a702c88514ce7f328f00a0d76d16593ed92d2
maihd <maihd.dev@gmail.com>
2021-09-20 02:34:11 +0700

    add some definition for LDtk parser

--- examples/LDtkParser/LDtkParser.h
+++ examples/LDtkParser/LDtkParser.h
@@ -7,31 +7,38 @@ typedef enum LDtkLayerType
 
 typedef struct LDtkEntity
 {
-    
+    const char* name;
 } LDtkEntity;
 
 typedef struct LDtkTile
 {
-
+    int x;
+    int y;
 } LDtkTile;
 
 typedef struct LDtkTileset
 {
-    
+    const char* name;
+    const char* path;
 } LDtkTileset;
 
 typedef struct LDtkLayer
 {
-
+    LDtkTileset tileset;
 } LDtkLayer;
 
 typedef struct LDtkIntGrid
 {
-    
+    int cols;
+    int rows;
+    int data[];
 } LDtkIntGrid;
 
 typedef struct LDtkWorld
 {
-
+    LDtkTileset*    tilesets;
+    LDtkLayer*      layers;
+    LDtkIntGrid*    grids;
+    LDtkEntity*     entities;
 } LDtkWorld;
 
