077cbc5bf6c793407e1cc7f7828ccb36396b17ee
maihd <maihd.dev@gmail.com>
2021-09-22 21:46:17 +0700

    load and parse sample ldtk file

--- examples/LDtkParser/Main.c
+++ examples/LDtkParser/Main.c
@@ -1,6 +1,9 @@
 #include "raylib.h"
 #include "LDtkParser.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #pragma comment(lib, "winmm.lib")
 
 int main(void)
@@ -9,8 +12,34 @@ int main(void)
 
     SetTargetFPS(60);
 
+    FILE* file;
+    errno_t fileError = fopen_s(&file, "../../examples/LDtkParser/assets/sample.ldtk", "r"); // This path is hardcode, rewrite if needed 
+    if (!file || fileError != 0)
+    {
+        fprintf(stderr, "Can not load sample file, maybe path is wrong!\n");
+        return -1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    int32_t fileSize = (int32_t)ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* json = (char*)malloc(fileSize + 1);
+    fread(json, 1, fileSize, file);
+    json[fileSize + 1] = 0;
+
+    fclose(file);
+
+    int32_t tempBufferSize = 1024 * 1024;
+    void* tempBuffer = malloc((size_t)tempBufferSize);
+
     LDtkWorld world;
-    LDtkError error = LDtkParse("", 0, NULL, 0, &world);
+    LDtkError error = LDtkParse(json, fileSize, tempBuffer, tempBufferSize, &world);
+    if (error.code != LDtkErrorCode_None)
+    {
+        fprintf(stderr, "Parse ldtk sample content failed!: %s\n", error.message);
+        return -1;
+    }
 
     while (!WindowShouldClose())
     {
