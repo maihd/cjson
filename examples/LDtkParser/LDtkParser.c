c4dd20954ee1ec9a3e02f29f5da00cb2aad877f1
maihd <maihd.dev@gmail.com>
2021-09-26 05:01:37 +0700

    make json natvis usable

--- examples/LDtkParser/LDtkParser.c
+++ examples/LDtkParser/LDtkParser.c
@@ -18,8 +18,6 @@ LDtkError LDtkParse(const char* content, int32_t contentLength, void* buffer, in
         return error;
     }
 
-    
-
     const LDtkError error = { LDtkErrorCode_None, "" };
     return error;
 }
