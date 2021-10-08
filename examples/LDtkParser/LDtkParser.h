8734a8132daaa3b0f83d03a415cc51a6ee78934b
maihd <maihd.dev@gmail.com>
2021-10-08 16:45:57 +0700

    LDtkParser: update tiles

--- examples/LDtkParser/LDtkParser.h
+++ examples/LDtkParser/LDtkParser.h
@@ -8,7 +8,6 @@
 
 typedef enum LDtkDirection
 {
-    LDtkDirection_None,
     LDtkDirection_North,
     LDtkDirection_East,
     LDtkDirection_South,
@@ -62,55 +61,163 @@ typedef struct LDtkTile
     bool        flipY;
 } LDtkTile;
 
+typedef struct LDtkIntGridValue
+{
+    const char* name;
+    int32_t     value;
+    LDtkColor   color;
+} LDtkIntGridValue;
+
 typedef struct LDtkTileset
 {
     const char* name;   // Name of tileset
     const char* path;   // Relative path to image file
 } LDtkTileset;
 
-typedef struct LDtkLayer
+typedef struct LDtkTileLayer
 {
+    int32_t     id;
     const char* name;
-
-    int32_t     order;
-
-    LDtkTileset tileset;
-} LDtkLayer;
-
-typedef struct LDtkIntGrid
-{
-    const char* name;
-
-    int32_t     order;
 
     int32_t     cols;
     int32_t     rows;
-    int32_t     data[];
-} LDtkIntGrid;
+    int32_t     cellSize;
 
-typedef struct LDtkEntityGroup
-{
-    const char* name;
+    int32_t     offsetX;
+    int32_t     offsetY;
+
+    float       tilePivotX;
+    float       tilePivotY;
 
     int32_t     order;
+    int32_t     visible : 1;
+    int32_t     opacity : 31;
+
+    LDtkTileset tileset;
+    
+    int32_t     tileCount;
+    LDtkTile*   tiles;
+} LDtkTileLayer;
+
+typedef struct LDtkIntGridLayer
+{
+    int32_t             id;
+    const char*         name;
+
+    int32_t             cols;
+    int32_t             rows;
+    int32_t             cellSize;
+
+    int32_t             offsetX;
+    int32_t             offsetY;
+
+    float               tilePivotX;
+    float               tilePivotY;
+
+    int32_t             order;
+    int32_t             visible : 1;
+    int32_t             opacity : 31;
+
+    int32_t             valueCount;
+    LDtkIntGridValue    values[];
+} LDtkIntGridLayer;
+
+typedef struct LDtkEntityLayer
+{
+    int32_t     id;
+    const char* name;
+
+    int32_t     cols;
+    int32_t     rows;
+    int32_t     cellSize;
+
+    int32_t     offsetX;
+    int32_t     offsetY;
+
+    float       tilePivotX;
+    float       tilePivotY;
+
+    int32_t     order;
+    int32_t     visible : 1;
+    int32_t     opacity : 31;
 
     int32_t     entityCount;
     LDtkEntity* entities;
-} LDtkEntityGroup;
+} LDtkEntityLayer;
 
 typedef struct LDtkLevel
 {
     const char*         name;
 
-    int32_t             layerCount;
-    LDtkLayer*          layers;
+    const char*         bgImagePath;
+    int32_t             bgImageX;
+    int32_t             bgImageY;
+    int32_t             bgImageCropX;
+    int32_t             bgImageCropY;
+    float               bgImageScaleX;
+    float               bgImageScaleY;
 
-    int32_t             gridCount;
-    LDtkIntGrid*        grids;
+    int32_t             tileLayerCount;
+    LDtkTileLayer*      tileLayers;
 
-    int32_t             entityGroupCount;
-    LDtkEntityGroup*    entityGroups;
+    int32_t             intGridLayerCount;
+    LDtkIntGridLayer*   intGridLayers;
+
+    int32_t             entityLayerCount;
+    LDtkEntityLayer*    entityLayers;
+
+    int32_t             neigbourIds;
+    struct LDtkLevel*   neigbours;
 } LDtkLevel;
+
+typedef enum LDtkLayerType
+{
+    LDtkLayerType_Tiles,
+    LDtkLayerType_IntGrid,
+    LDtkLayerType_Entities,
+    LDtkLayerType_AutoLayer,
+} LDtkLayerType;
+
+typedef struct LDtkLayerDef
+{
+    int32_t             id;
+    const char*         name;
+
+    LDtkLayerType       type;
+
+    int32_t             cellSize;
+    float               opacity;
+
+    int32_t             offsetX;
+    int32_t             offsetY;
+
+    float               tilePivotX;
+    float               tilePivotY;
+    LDtkTileset*        tileset;
+
+    int32_t             intGridValueCount;
+    LDtkIntGridValue*   intGridValues;
+} LDtkLayerDef;
+
+typedef struct LDtkEntityDef
+{
+    int32_t         id;
+    const char*     name;
+    
+    int32_t         rows;
+    int32_t         cols;
+
+    LDtkColor       color;
+    
+    float           pivotX;
+    float           pivotY;
+    
+    int32_t         tileId;
+    LDtkTileset*    tileset;
+
+    int32_t         tagCount;
+    const char**    tags;
+} LDtkEntityDef;
 
 typedef struct LDtkWorld
 {
@@ -127,11 +234,11 @@ typedef struct LDtkWorld
     int32_t         enumCount;
     LDtkEnum*       enums;
 
-    int32_t         layerCount;
-    LDtkLayer*      layers;
+    int32_t         layerDefCount;
+    LDtkLayerDef*   layerDefs;
 
-    int32_t         enityCount;
-    LDtkEntity*     entities;
+    int32_t         entityDefCount;
+    LDtkEntityDef*  entityDefs;
 
     int32_t         levelCount;
     LDtkLevel*      levels;
